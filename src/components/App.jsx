d134d43b4043a81dd1a73e790b19de4fe9743e62
Paulina-Maciesowicz <paulinamaa@gmail.com>
2023-06-01 23:51:26 +0200

    u

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,5 @@
+import { form } from './Input/input';
+
 export const App = () => {
   return (
     <div
@@ -10,6 +12,7 @@ export const App = () => {
         color: '#010101'
       }}
     >
+      <form />
       React homework template
     </div>
   );
---
+++ src/components/Input/input.jsx
@@ -0,0 +1,14 @@
+// import PropTypes from 'prop-types';
+// import css from './input.css';
+
+export const form = ({}) => {
+  return (
+    <input
+      type="text"
+      name="name"
+      pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+      title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+      required
+    />
+  );
+};
